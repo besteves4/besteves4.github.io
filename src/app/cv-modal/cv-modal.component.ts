38d76ee549c439dced06183d8323fd578892dd4a
Marta codes <marta.loureiro.s@gmail.com>
2020-03-29 20:01:19 +0100

    page changes

--- src/app/cv-modal/cv-modal.component.ts
+++ src/app/cv-modal/cv-modal.component.ts
@@ -5,6 +5,7 @@ import {
     Input
 } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { faLaptopCode, faGraduationCap, faCog, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-cv-modal',
@@ -15,6 +16,10 @@ export class CvModalComponent implements OnInit {
 
     @Input() job: any;
     @ViewChild('cvModal', {static: false}) modal;
+    faLaptopCode = faLaptopCode;
+    faGraduationCap = faGraduationCap;
+    faCog = faCog;
+    faCheck = faCheck;
     
     constructor() {}
    
